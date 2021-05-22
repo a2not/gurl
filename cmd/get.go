67a72b240342c216bd0275fab45b57ffcbf940a3
Khdbble <fb503d52.af1c4d@gmail.com>
2021-05-22 15:00:52 +0900

    implemented bare minimum get subcommand

--- cmd/get.go
+++ cmd/get.go
@@ -16,24 +16,23 @@ limitations under the License.
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	errUrlNotGiven = errors.New("url is not given as the first command line argument")
 )
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
-	},
+	Short: "execute HTTP GET request",
+	Long:  `gurl get is subcommand for sending HTTP GET request to a given url.`,
+	RunE:  httpget,
 }
 
 func init() {
@@ -48,4 +47,21 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func httpget(cmd *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return errUrlNotGiven
+	}
+	url := args[0]
+
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+
+	fmt.Printf("%v %v\n", resp.Proto, resp.Status)
+
+	for k, v := range resp.Header {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+	return nil
 }
