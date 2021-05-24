2f33be9e098df082f780f328bf6ff8aa1e49860c
Khdbble <fb503d52.af1c4d@gmail.com>
2021-05-24 18:42:33 +0900

    added header value check in testing

--- cmd/get_test.go
+++ cmd/get_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestGetURL(t *testing.T) {
@@ -19,6 +21,10 @@ func TestGetURL(t *testing.T) {
 				fmt.Fprintln(w, "Hello, client")
 			}),
 			wantErr: nil,
+			wantOut: []byte(`HTTP/1.1: 200 OK
+Content-Length: [14]
+Content-Type: [text/plain; charset=utf-8]
+`),
 		},
 	}
 
@@ -30,5 +36,41 @@ func TestGetURL(t *testing.T) {
 		if err := getURL(testServer.URL, buf); err != tt.wantErr {
 			t.Errorf("Error not match: want %v but got %v", tt.wantErr, err)
 		}
+
+		got := ignoreLines(buf.Bytes(), "Date")
+		fmt.Println(string(got))
+
+		if diff := cmp.Diff(tt.wantOut, got); diff != "" {
+			t.Errorf("Output string differ (-want +got): \n%v", diff)
+		}
 	}
+}
+
+func ignoreLines(b []byte, prefixs ...string) []byte {
+	set := make(map[string]struct{})
+	for _, k := range prefixs {
+		set[k] = struct{}{}
+	}
+
+	splitBytes := bytes.Split(b, []byte("\n"))
+	res := make([]byte, 0, len(b))
+
+	for _, line := range splitBytes {
+		kvp := bytes.Split(line, []byte(":"))
+		if len(kvp) < 2 {
+			continue
+		}
+
+		k := string(kvp[0])
+
+		if _, exist := set[k]; exist {
+			continue
+		}
+
+		fmt.Println(string(line))
+
+		res = append(res, line...)
+		res = append(res, []byte("\n")...)
+	}
+	return res
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/a2not/gurl
 go 1.16
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/spf13/cobra v1.1.3 // indirect
-	github.com/spf13/viper v1.7.1 // indirect
+	github.com/google/go-cmp v0.3.0
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
